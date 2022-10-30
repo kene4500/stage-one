b9049fe36c956c47d736c28c85d377e5b7dc8060
kene4500 <42975728+kene4500@users.noreply.github.com>
2022-10-30 17:31:55 +0100

    more updates

--- stage1/views.py
+++ stage1/views.py
@@ -2,5 +2,5 @@ from django.http import JsonResponse
 import json
 
 def getData(request):
-    info = { "slackUsername": "Kene4500", "backend": True, "age": 32, "bio": "I am in this program to upgrade my skillsets." }
+    info = { "slackUsername": "@Kene4500", "backend": True, "age": 32, "bio": "I am in this program to upgrade my skillsets." }
     return JsonResponse(info, safe=False)
